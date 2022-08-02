2cbcd8687c9c7fc441f5ce42a98f43bf0ca76690
truebluebamboo365 <51684345+truebluebamboo365@users.noreply.github.com>
2022-08-02 14:15:19 +0930

    Add files via upload
    Service bus topic sender & receiver

---
+++ Service bus - topic-receive/Service bus - topic-1/topic-receive/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace topic_receive
+{
+    class Program
+    {
+        private static string whizlab_connection_string = "Endpoint=sb://servicebusnamespace-1-endev.servicebus.windows.net/;SharedAccessKeyName=azure_topic_policy;SharedAccessKey=vv3BMdyrQRVq8eemmxaHIxNT6C7rrKY0ArJL1iMH73E=";
+        private static string whizlab_subscription = "topicsub";
+        private static ISubscriptionClient whizlab_client;
+        static void Main(string[] args)
+        {
+            Whizlab_receive().GetAwaiter().GetResult();
+        }
+
+        static async Task Whizlab_receive()
+        {
+            // Create a connection to the subscription
+            ServiceBusConnectionStringBuilder w_builder = new ServiceBusConnectionStringBuilder(whizlab_connection_string);
+            whizlab_client = new SubscriptionClient(w_builder, whizlab_subscription);
+
+            // Specify how the message will be received
+            var _options = new MessageHandlerOptions(whizlab_exception)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            // Register the function that will process the messages
+            whizlab_client.RegisterMessageHandler(whizlab_message, _options);
+            Console.ReadKey();
+
+        }
+
+        static async Task whizlab_message(Message whizlab_message, CancellationToken _token)
+        {
+            
+            Console.WriteLine(Encoding.UTF8.GetString(whizlab_message.Body));
+
+            // Receive a message from the topic
+            await whizlab_client.CompleteAsync(whizlab_message.SystemProperties.LockToken);
+        }
+
+        static Task whizlab_exception(ExceptionReceivedEventArgs args)
+        {
+            Console.WriteLine(args.Exception);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Service bus - topic-send/Service bus - topic/topic-send/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace topic_send
+{
+    class Program
+    {
+        private static string whizlab_connection_string = "Endpoint=sb://servicebusnamespace-1-endev.servicebus.windows.net/;SharedAccessKeyName=azure_topic_policy;SharedAccessKey=vv3BMdyrQRVq8eemmxaHIxNT6C7rrKY0ArJL1iMH73E=";
+        private static string whizlab_topic = "azure_6_qn";
+        private static ITopicClient whizlab_client;
+        static async Task Main(string[] args)
+        {
+            // Create a connection to the topic
+            whizlab_client = new TopicClient(whizlab_connection_string, whizlab_topic);
+
+            for (int i = 1; i <= 20; i++)
+            {
+                // Create a message
+                
+                var whizlab_message = new Message(Encoding.UTF8.GetBytes($"Message {i}"));
+                whizlab_message.MessageId = $"{i}";
+                Console.WriteLine($"Message : {i} sent");
+                // Send the message to the topic
+                await whizlab_client.SendAsync(whizlab_message);
+            }
+            }
+    }
+}
