ab9a1f5f97f2fe605be49ce875f902170fdb537d
truebluebamboo365 <51684345+truebluebamboo365@users.noreply.github.com>
2022-08-02 14:18:18 +0930

    Add files via upload
    Azure service bus queue send & receive

---
+++ whizlab-service bus - queue-send/whizlab-service bus - queue/queue-send/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue_send
+{
+    class Program
+    
+    {
+        // Remove the queue name
+        private static string whizlab_connection_string = "Endpoint=sb://servicebusnamespace-1-endev.servicebus.windows.net/;SharedAccessKeyName=azure_qn;SharedAccessKey=h1HMWOARMResns87rU6nISsbRapM5NboK4rYIKrVi9U=";
+        private static string whizlab_queue = "azure_5_qn";
+        static async Task Main(string[] args)
+        {
+            IQueueClient w_queueclient;
+            // Create a client connection to the queue
+            w_queueclient = new QueueClient(whizlab_connection_string, whizlab_queue);
+            Console.WriteLine("Sending Messages");
+            for (int i = 1; i <=20; i++)
+            {
+                // Create a new message
+                var whizlab_message = new Message(Encoding.UTF8.GetBytes($"a &amp; b {i}"));
+                
+                // Send a message to the queue
+                await w_queueclient.SendAsync(whizlab_message);
+                Console.WriteLine($"Message sent: a &amp; b {i}");
+            }
+        }
+
+    }
+}
+
---
+++ whizlab-service bus-queue-receive/whizlab-service bus-queue-1/queue-receive/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace queue_receive
+{
+    class Program
+    {
+        private static string whizlab_connection_string = "Endpoint=sb://servicebusnamespace-1-endev.servicebus.windows.net/;SharedAccessKeyName=azure_qn;SharedAccessKey=h1HMWOARMResns87rU6nISsbRapM5NboK4rYIKrVi9U=";
+        private static string whizlab_queue = "azure_5_qn";
+        private static QueueClient whizlab_client;
+
+        static void Main(string[] args)
+        {
+            Whizlab_receive().GetAwaiter().GetResult();
+        }
+
+        static async Task Whizlab_receive()
+        {
+            // Create a connection to the queue
+            whizlab_client = new QueueClient(whizlab_connection_string, whizlab_queue);
+
+            // Specify the options on how you will receive a message
+            var _options = new MessageHandlerOptions(whizlab_exception)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            // Register the function and that will receive the messages
+            whizlab_client.RegisterMessageHandler(whizlab_message, _options);
+            Console.ReadKey();
+
+        }
+
+        static async Task whizlab_message(Message whizlab_message, CancellationToken _token)
+        {
+            Console.WriteLine(Encoding.UTF8.GetString(whizlab_message.Body));
+            // Receive a message from the queue
+            await whizlab_client.CompleteAsync(whizlab_message.SystemProperties.LockToken);
+        }
+
+        static Task whizlab_exception(ExceptionReceivedEventArgs args)
+        {
+            Console.WriteLine(args.Exception);
+            return Task.CompletedTask;
+        }
+
+    }
+}
